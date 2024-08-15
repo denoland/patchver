b8ed2ea7ecb4a3f9e05257c067da647f84d4c7ec
Divy Srivastava <dj.srivastava23@gmail.com>
2024-08-15 18:24:15 +0530

    Update

--- main.rs
+++ main.rs
@@ -4,21 +4,45 @@ use libsui::PortableExecutable;
 
 use libsui::utils;
 
-const HELP: &str = r#"Usage: patchver <exe> <data_file> <output>"#;
-const SECTION: &str = "denoversion";
+const HELP: &str = r#"
+patchver
+
+USAGE:
+    patchver [OPTIONS] <input> <output>
+
+FLAGS:
+    -h, --help           Prints help information
+    --channel CHANNEL    Update release channel (stable, lts, canary, rc)
+
+ARGS:
+    <input>     Input file
+    <output>    Output file
+"#;
+
+const SECTION: &str = "denover";
+const CHANNELS: [&str; 4] = ["stable", "lts", "canary", "rc"];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 4 {
+    let mut pargs = pico_args::Arguments::from_env();
+
+    if pargs.contains(["-h", "--help"]) {
         eprintln!("{}", HELP);
         std::process::exit(1);
     }
 
-    let exe = std::fs::read(&args[1])?;
-    let data = std::fs::read(&args[2])?;
+    let exe: std::path::PathBuf = pargs.free_from_str().map_err(|_| "Missing input file")?;
+    let out: std::path::PathBuf = pargs.free_from_str().map_err(|_| "Missing output file")?;
 
-    let mut out = std::fs::File::create(&args[3])?;
+    let data = pargs.value_from_str::<&str, String>("--channel")?;
+    if !CHANNELS.contains(&data.as_str()) {
+        eprintln!("Invalid channel. Expected one of: stable, lts, canary, rc");
+        std::process::exit(1);
+    }
 
+    let exe = std::fs::read(&exe)?;
+    let mut out = std::fs::File::create(&out)?;
+
+    let data = data.as_bytes().to_vec();
     if utils::is_pe(&exe) {
         PortableExecutable::from(&exe)?
             .write_resource(SECTION, data)?
